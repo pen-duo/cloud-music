205bb1962dcdd7fbe7eedf96d62a165b101a43c7
pen-duo <2411363896@qq.com>
2021-07-09 10:27:35 +0800

    AppHeader布局和logo显示

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,11 +1,23 @@
 import React, { memo } from "react";
+
 import { NavLink } from "react-router-dom";
+import { HeaderWrapper, HeadLeft, HeadRight } from "./style";
 export default memo(function MHAppFooter() {
   return (
-    <div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeadLeft>
+          <a className="logo sprite_01" href="#/">
+            网易云音乐
+          </a>
+        </HeadLeft>
+        <HeadLeft>Right</HeadLeft>
+      </div>
+      <div className="divider"></div>
+
       <NavLink to="/">发现音乐</NavLink>
       <NavLink to="/mine">我的音乐</NavLink>
       <NavLink to="/friend">我的朋友</NavLink>
-    </div>
+    </HeaderWrapper>
   );
 });
---
+++ src/components/app-header/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+export const HeaderWrapper = styled.div`
+  height: 75px;
+  background-color: #242424;
+  .content {
+    height: 70px;
+    background: blue;
+    display: flex;
+    justify-content: space-between;
+  }
+  .divider {
+    height: 5px;
+    background-color: #c20c20;
+  }
+`;
+
+export const HeadLeft = styled.div`
+  .logo {
+    display: block;
+    width: 176px;
+    height: 70px;
+    background-position: 0 0;
+    text-indent: -9999px;
+  }
+`;
+export const HeadRight = styled.div``;
