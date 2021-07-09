fc1528cafe50e5174295e989cf5c4b4d0d6cbb2e
pen-duo <2411363896@qq.com>
2021-07-09 10:55:51 +0800

    AppHeader选项卡完成

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,23 +1,46 @@
 import React, { memo } from "react";
 
+import { headerLinks } from "../../common/loacl-data";
+
 import { NavLink } from "react-router-dom";
-import { HeaderWrapper, HeadLeft, HeadRight } from "./style";
+import { AppHeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 export default memo(function MHAppFooter() {
+  const showSelectedItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink exact to={item.link}>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
+      );
+    } else {
+      return (
+        <a href={item.link} target="_blank" rel="noopener noreferrer">
+          {item.title}
+        </a>
+      );
+    }
+  };
   return (
-    <HeaderWrapper>
+    <AppHeaderWrapper>
       <div className="content wrap-v1">
-        <HeadLeft>
+        <HeaderLeft>
           <a className="logo sprite_01" href="#/">
             网易云音乐
           </a>
-        </HeadLeft>
-        <HeadLeft>Right</HeadLeft>
+          <div className="select-list">
+            {headerLinks.map((item, index) => {
+              return (
+                <div className="select-item" key={item.title}>
+                  {showSelectedItem(item, index)}
+                </div>
+              );
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>Right</HeaderRight>
       </div>
       <div className="divider"></div>
-
-      <NavLink to="/">发现音乐</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-      <NavLink to="/friend">我的朋友</NavLink>
-    </HeaderWrapper>
+    </AppHeaderWrapper>
   );
 });
