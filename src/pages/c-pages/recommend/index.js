4eef7b0e58f0660624b03e0f32d5ae7529c50a90
pen-duo <2411363896@qq.com>
2021-07-11 08:13:41 +0800

    react-redux中hook的使用

--- src/pages/c-pages/recommend/index.js
+++ src/pages/c-pages/recommend/index.js
@@ -1,28 +1,47 @@
 import React, { memo, useEffect } from "react";
 import { getTopBannerAction } from "./store/actionCreators";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 function MHRecommend(props) {
-  const { getBanners, topBanners } = props;
+  //  获取数据,进行dispatch操作
+  const { topBanners } = useSelector((state) => {
+    return {
+      topBanners: state.recommend.topBanners,
+    };
+  });
+  const dispatch = useDispatch();
+  // 发送网络请求
   useEffect(() => {
-    getBanners();
-  }, [getBanners]);
+    dispatch(getTopBannerAction());
+  }, [dispatch]);
   return (
     <div>
-      <h2>recommend</h2>
+      <h2>{topBanners.length}</h2>
     </div>
   );
 }
-const mapStateToProps = (state) => {
-  return {
-    topBanners: state.recommend.topBanners,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    // 发起轮播图数据网络请求
-    getBanners() {
-      dispatch(getTopBannerAction());
-    },
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(memo(MHRecommend));
+
+// function MHRecommend(props) {
+//   const { getBanners, topBanners } = props;
+//   useEffect(() => {
+//     getBanners();
+//   }, [getBanners]);
+//   return (
+//     <div>
+//       <h2>recommend</h2>
+//     </div>
+//   );
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     topBanners: state.recommend.topBanners,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     getBanners() {
+//       dispatch(getTopBannerAction());
+//     },
+//   };
+// };
+export default memo(MHRecommend);
--- src/pages/c-pages/recommend/store/actionCreators.js
+++ src/pages/c-pages/recommend/store/actionCreators.js
@@ -1,14 +1,12 @@
 import * as actionTypes from "./constants";
 import { getTopBanner } from "@/services/recommend";
 
-// 派发action
 export const changeTopBannerAction = (res) => {
   return {
     type: actionTypes.CHANGE_TOP_BANNERS,
     topBanners: res.banners,
   };
 };
-// 获取轮播图数据
 export const getTopBannerAction = () => {
   return (dispatch) => {
     getTopBanner().then((res) => {
